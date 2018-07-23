71125614e88fb8e588055354d89d405083626d0f
16046562 <16046562@myrp.edu.sg>
2018-07-23 12:08:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16046562/demoswiper/FragmentPagerAdapter.java
@@ -0,0 +1,4 @@
+package com.example.a16046562.demoswiper;
+
+public class FragmentPagerAdapter {
+}
---
+++ app/src/main/java/com/example/a16046562/demoswiper/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.a16046562.demoswiper;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList <Fragment> al;
+    MyFragmentPagerAdapter adapter;
+    ViewPager vPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        vPager = (ViewPager) findViewById(R.id.viewpager1);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        al = new ArrayList<Fragment>();
+        al.add(new Frag1());
+        al.add(new Frag2());
+
+        adapter = new MyFragmentPagerAdapter(fm, al);
+
+        vPager.setAdapter(adapter);
+    }
+
+}
